34d2380c057ae58fcde6daa0bc99c6c42a7b8d51
ermoya <ermoya@minsait.com>
2023-05-24 23:21:36 +0200

    fix: resuelve conflicto dependencia deploy/Tomcat

--- src/main/java/es/erumo/poc/swagger/Application.java
+++ src/main/java/es/erumo/poc/swagger/Application.java
@@ -2,6 +2,7 @@ package es.erumo.poc.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class Application extends SpringBootServletInitializer {
 
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+    return builder.sources(Application.class);
   }
 
 }
