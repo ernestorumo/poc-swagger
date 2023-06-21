4f4ab55d4e027fd8e713d6876ec16221b4056c42
ermoya <ermoya@minsait.com>
2023-06-21 19:05:11 +0200

    feat: incorpora ejemplo propiedad tamaño dato

--- src/main/java/es/erumo/poc/swagger/controller/AnimalController.java
+++ src/main/java/es/erumo/poc/swagger/controller/AnimalController.java
@@ -3,7 +3,9 @@ package es.erumo.poc.swagger.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,12 +27,13 @@ import es.erumo.poc.swagger.model.AnimalResponse;
 @RequestMapping("/api/v1")
 public class AnimalController implements AnimalOperation {
 
+  @Value("${crawler.max-body-size}")
+  DataSize tal;
+
   @Override
   @GetMapping(value = "/animales/{id}", produces = "application/json")
   public ResponseEntity<AnimalResponse> getAnimalById(@PathVariable("id") final Long id) {
-
     return ResponseEntity.ok(new AnimalResponse(id, "tigre"));
-
   }
 
   @Override
